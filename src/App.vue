c172f296ef5ceaa614505acf46f96f34b24dcb57
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2023-07-20 13:11:13 +0530

    Send data by post method in Vue Js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>POST method API</h1>
+<input type="text" name="email" v-model="email">
+<br><br>
+<input type="text" name="password" v-model="password">
+<input type="button" @click="addUser()" value="Add User">
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+ data()
+ {
+  return{
+    email:'',
+    password:''
   }
+ },
+ methods:{
+  addUser()
+  {
+    console.warn(this.email,this.password);
+  }
+ }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
